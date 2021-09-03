25750bb16793d336d56445eaa3643919b859b17a
maximkovalev <mkovalevhse@yandex.ru>
2021-09-03 17:26:18 +0300

    21

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@
 #python manage.py collectstatic
 
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin','admin@example.ru','admin')" | python manage.py shell
-gunicorn reportEpar.wsgi:application -b 0.0.0.0:8000 --reload
+gunicorn reportEpar.wsgi:application -b 0.0.0.0:4000 --reload
