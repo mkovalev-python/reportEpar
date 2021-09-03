d228fc95a4c534bc8511670e3257579b7ad5b96c
maximkovalev <mkovalevhse@yandex.ru>
2021-09-03 21:47:27 +0300

    debug false

--- reportEpar/settings.py
+++ reportEpar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ndhuytgr)_6$(9$cx!1telhv4ya!4+s=f+%ychacmb=-+9ys2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['94.26.245.131',]
 
